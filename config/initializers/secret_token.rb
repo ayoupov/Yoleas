1ee324a17d05a2239e5d3ce78950ed395c9cfd8a
Alexander Ayoupov <ayoupov@gmail.com>
2011-04-02 19:04:32 +0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yoleas::Application.config.secret_token = '109500742172e540506b33784fdd2ac5d1a4300fb04d0d46ee6985836d248ed396127d8582cf9f93a36bce482edba34e37c5baeed7f24e6d62981d71b1b614a5'
