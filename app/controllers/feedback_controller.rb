48d360419a8891b09d48e89d1719f496823b8495
Alexander Ayoupov <ayoupov@gmail.com>
2011-04-19 03:15:56 +0600

    stable(though nothing), i18n added

---
+++ app/controllers/feedback_controller.rb
@@ -0,0 +1,11 @@
+class FeedbackController < ApplicationController
+  def index
+   @posts = Post.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @posts }
+    end
+
+  end
+end
